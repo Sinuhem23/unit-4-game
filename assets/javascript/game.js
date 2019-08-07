ee4b859c5fb99fd85db85a83b62db2754118e257
sinuhe Montero <sinuhem23@gmail.com>
2019-08-07 00:42:14 -0400

    unit-4-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// variables for all.
+var name;
+var YourCharacter;
+var YourDefender;
+var myPlayer = "";
+var myDef = "";
+
+var attack;
+var attackcharacter;
+var attackerHP;
+var attackerAP;
+var attackerCAP;
+
+var defend;
+var defendcharacter;
+var defenderHP;
+var defenderAP;
+var defenderCAP;
